b43eb5f42acc195e02d508a3668be1dbf0d10bc5
Drimes <drimesbot@gmail.com>
2026-02-09 18:53:48 +0100

    fix(v1): use dynamic basename and asset rewrites

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+// Dynamically determine basename based on current URL
+// If we are at /v1 (main site subpath), use /v1
+// If we are at / (v1 site root), use /
+const basename = window.location.pathname.startsWith("/v1") ? "/v1" : "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/v1">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
